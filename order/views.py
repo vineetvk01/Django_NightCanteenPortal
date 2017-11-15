41dc4eee8f46f621de62754131ef519bfd2c0b90
vineet <vineetvk01@gmail.com>
2017-11-15 22:09:53 +0530

    ordersbase

--- order/views.py
+++ order/views.py
@@ -6,3 +6,18 @@ def index(request):
     all_foods = Food_items.objects.all()
     context = { 'all_foods' : all_foods }
     return render(request, 'order/index.html',context)
+
+def makeorder(request):
+    all_foods = Food_items.objects.all()
+    context = { 'all_foods' : all_foods }
+    return render(request, 'order/makeorder.html', context)
+
+def payorder(request):
+    all_foods = Food_items.objects.all()
+    context = { 'all_foods' : all_foods }
+    return render(request, 'order/payorder.html', context)
+
+def doneorder(request):
+    all_foods = Food_items.objects.all()
+    context = { 'all_foods' : all_foods }
+    return render(request, 'order/doneorder.html', context)
